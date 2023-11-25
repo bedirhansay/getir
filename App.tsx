01844d361471a16a8470eb8726b32434b84e0e1f
bedirhansay <bedirhan.sayy@gmail.com>
2023-11-25 13:49:06 +0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="text-red-600 mt-20 bg-red-500">
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
